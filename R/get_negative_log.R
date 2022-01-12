da8424128212a940ccd1a465e2c115c1abab855d
Angel Luis Robles Fernandez (Student) <alroble8@sundevils.asu.edu>
2022-01-12 08:30:11 -0700

    create a function to get the negative log-likelihood value from quadratic forms

---
+++ R/get_negative_log.R
@@ -0,0 +1,17 @@
+#  negative log-likelihood value
+#' Get the negative log-likelihood value from quadratic forms of environmental information from presence points and a sample from an M hypothesis
+#' @param q1  quadratic terms of presence points
+#' @param q2 quadratic terms of M points
+#'
+#' @return A single value of the negative log-likelehood
+#' @export
+#'
+#' @examples
+#' par <- get_ellip_par(spOccPnts)
+#' q1 <- apply(spOccPnts, 1, quad, par$mu, par$A)
+#' q2 <- apply(samMPts, 1, quad, par$mu, par$A) # quadratic terms of M points
+#' get_negative_log(q1, q2)
+get_negative_log <- function(q1, q2){
+  n <- length(q1)
+  0.5*sum(q1) + n*log(sum(exp(-0.5*q2)))
+}
