fdd38f1f45fd3477ca1247ac90170e1a36cd6c0f
Angel Luis Robles Fernandez (Student) <alroble8@sundevils.asu.edu>
2022-01-12 08:31:17 -0700

    Create a function that get the negative of  log-likelihood value

---
+++ R/negloglike_multivariable.R
@@ -0,0 +1,27 @@
+#' Negative log likelyhood
+#'
+#' @param guess A list of length 2, whit two objets, a vector mu of parameters, And a matrix A as the inverse of covariance matrix
+#' @param sam1 A matrix containing the original sample of environmental combinations that correspond to presences
+#' @param sam2 matrix containing a second random sample of environmental combinations which come from the area of study (M)
+#'
+#' @return A negative log likelyhood value
+#' @export
+#'
+#' @examples
+#' par <- get_ellip_par(spOccPnts)
+#' negloglike_multivariable(par, spOccPnts, samMPts)
+negloglike_multivariable <- function(guess, sam1, sam2){
+
+  # define the parameters of interest from the guess parameter
+  mu <- guess$mu
+  A <- guess$A
+  # original sample size: number of presence points in the sample
+  n <- nrow(sam1)
+
+  q1 <- apply(sam1, 1, quad, mu, A) # quadratic terms of presence points
+  q2 <- apply(sam2, 1, quad, mu, A) # quadratic terms of M points
+
+  # negative log-likelihood value
+  S <- 0.5*sum(q1) + n*log(sum(exp(-0.5*q2)))
+  return(S)
+}
