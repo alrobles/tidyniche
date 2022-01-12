b899be7c3cdfc461a565c30e4ba24d7423ddbe5a
Angel Luis Robles Fernandez (Student) <alroble8@sundevils.asu.edu>
2022-01-12 08:28:54 -0700

    Creating function to generate the inverse of covariance matrix.

---
+++ R/get_A_matrix.R
@@ -0,0 +1,14 @@
+#' Calculates the inverse of covariance matrix from environmental variables
+#' @import stats
+#' @param df A matrix or data frame with environmental variables
+#'
+#' @return A covariance matrix
+#' @export
+#'
+#' @examples
+#' get_A_matrix(spOccPnts)
+get_A_matrix <-  function(df){
+  Sig.ini <- stats::cov(df)
+  A.ini <- chol2inv(chol(Sig.ini))
+  A.ini
+}
